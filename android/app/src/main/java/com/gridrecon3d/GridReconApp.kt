19743d7e604f763895c702d6aff83d183e614287
Austin Michael <austinmsells@gmail.com>
2026-02-20 21:31:26 -0500

    Add navigation shell

--- android/app/src/main/java/com/gridrecon3d/GridReconApp.kt
+++ android/app/src/main/java/com/gridrecon3d/GridReconApp.kt
@@ -1,14 +1,9 @@
 package com.gridrecon3d
 
 import androidx.compose.runtime.Composable
-import androidx.compose.material3.*
-import androidx.compose.foundation.layout.*
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.material3.Text
 
 @Composable
 fun GridReconApp() {
-    Surface(modifier = Modifier.fillMaxSize()) {
-        MainScreen()
-    }
+    Text("OLD SCREEN REMOVED")
 }
